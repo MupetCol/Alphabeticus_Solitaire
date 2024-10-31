96a2200349113478df37b7e29df3fa87ce111348
Daniel "Mupet" Muñoz Pérez <mupet.col@gmail.com>
2024-10-31 17:49:01 -0500

    Tweaks

--- Assets/SimpleSolitaire/Resources/Scripts/Controller/Klondike/KlondikeCardLogic.cs
+++ Assets/SimpleSolitaire/Resources/Scripts/Controller/Klondike/KlondikeCardLogic.cs
@@ -31,6 +31,8 @@ namespace SimpleSolitaire.Controller
         [SerializeField] private Toggle _threeDrawRuleToggle;
         private KlondikeStatisticsController KlondikeStatisticsController => StatisticsComponent as KlondikeStatisticsController;
 
+        [SerializeField] private bool _mobile;
+
         private void ChangeRuleTypeByToggle(DeckRule rule)
         {
             if (CurrentRule == rule) return;
@@ -117,7 +119,7 @@ namespace SimpleSolitaire.Controller
                         }
 					}
 
-					int aces = 2;
+					int aces = 3;
 					int kings = 2;
 					int[] fronPosIndex = { 51, 49, 46, 42, 37, 31, 24 };
 					List<int> replacedOnes = new List<int>();
@@ -262,7 +264,7 @@ namespace SimpleSolitaire.Controller
 						}
 					}
 
-					int aces = 3;
+					int aces = 4;
                     int kings = 2;
 					int[] fronPosIndex = { 51, 49, 46, 42, 37, 31, 24 };
 					List<int> replacedOnes = new List<int>();
@@ -355,46 +357,6 @@ namespace SimpleSolitaire.Controller
 						}
 					}
 
-					for (int i = CardNumberArray.Length - 1; i > 0; i--)
-					{
-
-						if (replacedOnes.Contains(i) || fronPosIndex.Contains(i))
-						{
-							//For already replaced aces or aces on valid positions
-							continue;
-						}
-
-
-						if (CardNumberArray[i] == 6 || CardNumberArray[i] % 13 == 6)
-						{
-
-							int indexToReplace;
-							while (true)
-							{
-								indexToReplace = fronPosIndex[UnityEngine.Random.Range(0, fronPosIndex.Length)];
-								if (!replacedOnes.Contains(indexToReplace))
-								{
-									replacedOnes.Add(indexToReplace);
-									break;
-								}
-							}
-
-							int currentCardValue = CardNumberArray[i];
-							int cardForReplaceValue = CardNumberArray[indexToReplace];
-
-							/*Debug.Log("Replacing index " + i + "which is ace with value of" + CardNumberArray[i] +
-								"with index " + indexToReplace + "with value of " + CardNumberArray[indexToReplace]);*/
-
-							CardNumberArray[indexToReplace] = currentCardValue;
-							CardNumberArray[i] = cardForReplaceValue;
-
-                            break;
-							/* Test debug.
-							Debug.LogError($"lastReplaceIndex {lastReplaceIndex} replaceAmount {replaceAmount} Replace {currentCardValue} with {cardForReplaceValue} ");
-							*/
-						}
-					}
-
 					break;
 				}
 			}
@@ -404,9 +366,18 @@ namespace SimpleSolitaire.Controller
         {
             base.InitializeSpacesDictionary();
 
-            SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_VERTICAL_BOTTOM_OPENED, DeckHeight / 3.5f / 1.8f);
-            SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_VERTICAL_BOTTOM_CLOSED, DeckHeight / 3.5f / 2);
-            SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_HORIONTAL_WASTE, DeckHeight / 3.5f / 1.8f);
+            if (_mobile)
+            {
+				SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_VERTICAL_BOTTOM_OPENED, DeckHeight / 3.5f);
+				SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_VERTICAL_BOTTOM_CLOSED, DeckHeight / 3.5f / 2);
+				SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_HORIONTAL_WASTE, DeckHeight / 3.5f / 1.8f);
+			}
+            else
+            {
+				SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_VERTICAL_BOTTOM_OPENED, DeckHeight / 3.5f / 1.8f);
+				SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_VERTICAL_BOTTOM_CLOSED, DeckHeight / 3.5f / 2);
+				SpacesDict.Add(DeckSpacesTypes.DECK_SPACE_HORIONTAL_WASTE, DeckHeight / 3.5f / 1.8f);
+			}
         }
 
         public override void SubscribeEvents()
